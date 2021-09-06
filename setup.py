fdc950cf28e403841d1c269275b781bbd8244aa0
Roberta Takenaka <roberta.takenaka@scielo.org>
2021-09-06 11:45:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requires = [
 
 setup(
     name="exportsci",
-    version='0.6',
+    version='0.6.1',
     description="Export metadata to SciELO CI",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
